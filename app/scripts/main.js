17415e9c3430fafb6591805e215741d3d9d585e3
Zach Wolfe <wolfe.zach@gmail.com>
2015-01-04 14:30:50 -0800

    Add list of addressible groups

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,8 +5,13 @@ $(function() {
   // individually addressible LED groups, representing 'words' on the word clock
   function getAddressibleClasses() {
     // ignoredClasses serves two purposes:
-    // 1) ignore a few classes that are used
+    // 1) ignore a few classes that are used for display like
+    // 2) filter out duplicates 
     var ignoredClasses = {
+      'active': true,
+      'minute': true,
+      'hour': true,
+      'extra': true
     };
     var classes = [].map
           .call($('#world-clock td'),function(obj) { return $(obj).attr('class').split(' '); }) /* get array of arrays of classes for each td */
@@ -14,8 +19,8 @@ $(function() {
           .filter(function(obj) { return ignoredClasses.hasOwnProperty(obj) ? false : (ignoredClasses[obj] = true); }); /* filter out duplicates */
     return classes;
   }
- 
-  getAddressibleClasses();
+
+  window.getAddressibleClasses = getAddressibleClasses;
 
   var numCells = $('td').length;
   var numUsableCells = numCells - $('td.extra').length;
@@ -28,4 +33,13 @@ $(function() {
   $('#numUsableCells').text(numUsableCells);
   $('#numRows').text(numRows);
   $('#numCols').text(numCols);
+
+  var addressibleGroups = getAddressibleClasses();
+  $('#numAddressibleGroups').text(addressibleGroups.length);
+  addressibleGroups.forEach(function(group) {
+    $('#addressibleGroupsList').append(
+      $('<li>' + group + '</li>')
+      );
+  });
+
 });
